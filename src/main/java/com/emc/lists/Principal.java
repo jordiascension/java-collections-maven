238ec10b97fc34ce991a29aee7a81d7bd000888b
jordiascension <jordiascension@enterpriseconsulting.es>
2022-11-09 10:38:30 +0100

    Added generic list with lambda expressions. closes #3

--- src/main/java/com/emc/lists/Principal.java
+++ src/main/java/com/emc/lists/Principal.java
@@ -1,30 +1,17 @@
 package com.emc.lists;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Student> studentList = new ArrayList<Student>();
+		List<Student> studentList = StudentList.CreateList();
 
-		Student student = new Student();
-		student.setId(1);
-		student.setName("Pepe");
+		List<Student> studentSpainList = StudentList
+				.getStudentsByCountry(studentList, "Spain");
 
-		studentList.add(student);
-
-		Student student1 = new Student();
-		student1.setId(2);
-		student1.setName("Alberto");
-
-		studentList.add(student1);
-
-		for (Student student2 : studentList) {
-			System.out.println(student2.getId());
-			System.out.println(student2.getName());
-		}
+		StudentList.loopThroughList(studentSpainList);
 
 	}
 
